bfc17d1dea81de4273c93e7605619e3a5f6e5a75
gb1609 <gb1609@gb1609-Strix-GL504GM-GL504GM>
2019-05-05 14:07:05 +0200

    Finish test ex 1

--- APSD/src/Excercise1.h
+++ APSD/src/Excercise1.h
@@ -8,119 +8,140 @@
 #ifndef EXCERCISE1_H_
 #define EXCERCISE1_H_
 
-class Excercise1 {
-private:
-	int dim;
-	int num_threads;
-	double **matrixA;
-	double **matrixB;
-	double **parallel_matrixA = (double **) malloc(dim * sizeof(double));
-	double **parallel_matrixB = (double **) malloc(dim * sizeof(double));
-	FileWriter fw;
-public:
-	Excercise1(int d, int nt, FileWriter fw): dim(d),num_threads(nt), fw(fw) {
-		matrixA = (double **) malloc(dim * sizeof(double*));
-		matrixB = (double **) malloc(dim * sizeof(double*));
-		parallel_matrixA = (double **) malloc(dim * sizeof(double));
-		parallel_matrixB = (double **) malloc(dim * sizeof(double));
-		for (int i = 0; i < dim; i++) {
-			matrixA[i] = (double *) malloc(dim * sizeof(double));
-			matrixB[i] = (double *) malloc(dim * sizeof(double));
-			parallel_matrixA[i] = (double *) malloc(dim * sizeof(double));
-			parallel_matrixB[i] = (double *) malloc(dim * sizeof(double));
-		}
-	}
+class Excercise1
+{
+  private:
+  int dim;
+  int num_threads;
+  double serial;
+  double **matrixA;
+  double **matrixB;
+  double **parallel_matrixA = (double **) malloc(dim * sizeof(double));
+  double **parallel_matrixB = (double **) malloc(dim * sizeof(double));
+  FileWriter fw;
+  public:
+  Excercise1(int d, FileWriter fw): dim(d), fw(fw)
+  {
+    serial=0.0;
+    num_threads=0;
+    matrixA = (double **) malloc(dim * sizeof(double*));
+    matrixB = (double **) malloc(dim * sizeof(double*));
+    parallel_matrixA = (double **) malloc(dim * sizeof(double));
+    parallel_matrixB = (double **) malloc(dim * sizeof(double));
+    for (int i = 0; i < dim; i++)
+    {
+      matrixA[i] = (double *) malloc(dim * sizeof(double));
+      matrixB[i] = (double *) malloc(dim * sizeof(double));
+      parallel_matrixA[i] = (double *) malloc(dim * sizeof(double));
+      parallel_matrixB[i] = (double *) malloc(dim * sizeof(double));
+    }
+  }
 
-	void printMatrix(double **matrix, int dim) {
-		printf("%d", dim);
-		for (int a = 0; a < dim; a++) {
-			for (int b = 0; b < dim; b++) {
-				printf("%.8g \t", matrix[a][b]);
-			}
-			printf("\n");
-		}
-	}
+  void printMatrix(double **matrix, int dim)
+  {
+    printf("%d", dim);
+    for (int a = 0; a < dim; a++)
+    {
+      for (int b = 0; b < dim; b++)
+      {
+	printf("%.8g \t", matrix[a][b]);
+      }
+      printf("\n");
+    }
+  }
 
-	void execute()
-	{
-		double serial=serial_execute();
-		double parallel=parallel_execute();
-		check_equality();
-		printf("SPEED_UP:%.4g\n",serial/parallel);
-		free(matrixA);
-		free(matrixB);
-		free(parallel_matrixA);
-		free(parallel_matrixB);
-	}
+  void execute(int num_thread)
+  {
+    num_threads=num_thread;
+    double parallel=parallel_execute();
+    check_equality();
+    double speed_up = serial / parallel;
+    printf("NT: %d,SERIAL: %.8g,PARALLEL: %.8g,SPEED_UP:%.4g\n",num_thread,serial,parallel,speed_up);
+    fw.update_string(num_threads,serial,parallel,speed_up,"ex1 static");
+  }
 
-	double serial_execute() {
-		int a, b;
-		double time_serial, time_begin, time_serial_check;
-		time_begin = omp_get_wtime();
-		for (a = 0; a < dim; a++)
-		for (b = 0; b < dim; b++) {
-			matrixA[a][b] = 5 * pow(a, 3) + 5 * M_PI * pow(b, 6);
-			matrixB[a][b] = (10 / 3) * (matrixA[a][b]);
-		}
-		//	printMatrix(matrixA, dim);
-		time_serial = omp_get_wtime() - time_begin;
-		printf(
-				"*TIME SERIAL EXECUTION: %8g\n*",
-				time_serial);
-		return time_serial;
-	}
+  void write()
+  {
+    fw.write();
+  }
 
-	double parallel_execute() {
-		int a,b;
-		double end_time,begin_time;
-		omp_set_num_threads(num_threads);
-		begin_time=omp_get_wtime();
+  void clean()
+  {
+    free(matrixA);
+    free(matrixB);
+    free(parallel_matrixA);
+    free(parallel_matrixB);
+  }
+
+  void serial_execute()
+  {
+    int a, b;
+    double time_serial, time_begin, time_serial_check;
+    time_begin = omp_get_wtime();
+    for (a = 0; a < dim; a++)
+    for (b = 0; b < dim; b++)
+    {
+      matrixA[a][b] = 5 * pow(a, 3) + 5 * M_PI * pow(b, 6);
+      matrixB[a][b] = (10 / 3) * (matrixA[a][b]);
+    }
+    //	printMatrix(matrixA, dim);
+    time_serial = omp_get_wtime() - time_begin;
+    printf(
+	"*TIME SERIAL EXECUTION: %8g\n*",
+	time_serial);
+    serial=time_serial;
+  }
+
+  double parallel_execute()
+  {
+    int a,b;
+    double end_time,begin_time;
+    omp_set_num_threads(num_threads);
+    begin_time=omp_get_wtime();
 #pragma omp parallel private(a, b)
-		{
-#pragma omp for schedule(dynamic)
-			for (a = 0; a < dim; a++) {
-				for (b = 0; b < dim; b++) {
-					parallel_matrixA[a][b] = 5 * pow(a, 3) + 5 * M_PI * pow(b, 6);
-					parallel_matrixB[a][b] = (10 / 3) * (matrixA[a][b]);
-				}
-			}
-		}
-		end_time = omp_get_wtime() - begin_time;
-		printf(
-				"*TIME PARALLEL EXECUTION: %8g\n*",
-				end_time);
-		return end_time;
-	}
-
-	void check_equality()
+    {
+#pragma omp for
+      for (a = 0; a < dim; a++)
+      {
+	for (b = 0; b < dim; b++)
 	{
-		double end_time,begin_time;
-		begin_time=omp_get_wtime();
-		int finalSum = 0, a, b;
-#pragma omp parallel for shared(matrixA,parallel_matrixA) private(a,b) reduction(+:finalSum)
-			for (a = 0; a < dim; a++)
-				for (b = 0; b < dim; b++)
-					if (matrixA[a][b] != parallel_matrixA[a][b])
-						finalSum++;
-		int sum=finalSum;
-		finalSum=0;
-#pragma omp parallel for shared(matrixB,parallel_matrixB) private(a,b) reduction(+:finalSum)
-			for (a = 0; a < dim; a++)
-			for (b = 0; b < dim; b++)
-			if (matrixB[a][b] != parallel_matrixB[a][b])
-			finalSum++;
-
-		finalSum+=sum;
-		if(finalSum>0)
-		printf("*ERROR\n");
-		else
-		printf("*CORRECT, SAME VALUES\t");
-		end_time=omp_get_wtime()-begin_time;
-		printf("TIME CHECKING: %.8g*\n",end_time);
+	  parallel_matrixA[a][b] = 5 * pow(a, 3) + 5 * M_PI * pow(b, 6);
+	  parallel_matrixB[a][b] = (10 / 3) * (matrixA[a][b]);
 	}
+      }
+    }
+    end_time = omp_get_wtime() - begin_time;
+    return end_time;
+  }
+
+  void check_equality()
+  {
+    double end_time,begin_time;
+    begin_time=omp_get_wtime();
+    int finalSum = 0, a, b;
+#pragma omp parallel for shared(matrixA,parallel_matrixA) private(a,b) reduction(+:finalSum)
+    for (a = 0; a < dim; a++)
+    for (b = 0; b < dim; b++)
+    if (matrixA[a][b] != parallel_matrixA[a][b])
+    finalSum++;
+    int sum=finalSum;
+    finalSum=0;
+#pragma omp parallel for shared(matrixB,parallel_matrixB) private(a,b) reduction(+:finalSum)
+    for (a = 0; a < dim; a++)
+    for (b = 0; b < dim; b++)
+    if (matrixB[a][b] != parallel_matrixB[a][b])
+    finalSum++;
+
+    finalSum+=sum;
+    if(finalSum>0)
+    printf("*ERROR\n");
+    else
+    printf("*CORRECT, SAME VALUES\t");
+    end_time=omp_get_wtime()-begin_time;
+    printf("TIME CHECKING: %.8g*\n",end_time);
+  }
 
 };
 
 #endif /* EXCERCISE1_H_ */
-
 
--- APSD/src/apsd_Spring_Bonus.cpp
+++ APSD/src/apsd_Spring_Bonus.cpp
@@ -20,14 +20,21 @@ main ()
 {
   int toLaunch, num_thread, dim;
   const int max_num_threads = omp_get_max_threads ();
-  dim = 5000;
-  toLaunch = 5;
+  dim = 20000;
+  toLaunch = 1;
   FileWriter fw (toLaunch);
   switch (toLaunch)
   {
     case 1:
+    {
       printf ("***Exercise 1*** \n");
-      Excercise1 (dim, num_thread, fw).execute ();
+      Excercise1 ex1 = Excercise1 (dim, fw);
+      ex1.serial_execute();
+      for (num_thread = 2; num_thread < max_num_threads + 1; num_thread++)
+	ex1.execute (num_thread);
+      ex1.write();
+      ex1.clean();
+    }
       break;
     case 2:
       printf ("***Exercise 2***\n");
@@ -38,8 +45,11 @@ main ()
       Excercise3 (dim, num_thread, fw).execute ();
       break;
     case 4:
+    {
       printf ("***Exercise 4*** \n");
-      Excercise4 (dim, num_thread, fw).execute ();
+      Excercise4 ex4 (dim, num_thread, fw);
+      ex4.execute ();
+    }
       break;
     case 5:
     {
@@ -51,7 +61,7 @@ main ()
 	ex5.execute (num_thread);
 	ex5.reset_parallel_matrix ();
       }
-      ex5.write();
+      ex5.write ();
       ex5.clean ();
     }
       break;
